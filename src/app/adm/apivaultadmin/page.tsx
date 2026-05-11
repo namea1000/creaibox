c25de2438365ad1b975da55c3c4133ad6a8271b9
namea1000 <jenam7720@gmail.com>
2026-05-11 22:40:14 +0900

    오류수정

---
+++ src/app/adm/apivaultadmin/page.tsx
@@ -0,0 +1,10 @@
+import APIValutAdmin from "@/components/vault/APIValutAdmin";
+
+export default function APIValutAdminPage() {
+  return (
+    <main>
+      {/* 사용자용 로직 */}
+      <APIValutAdmin />
+    </main>
+  );
+}
