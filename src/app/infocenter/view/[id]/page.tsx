adb33d79b6fb556e91012f73045cbe45a03cb6be
namea1000 <jenam7720@gmail.com>
2026-05-13 02:08:21 +0900

    info

--- src/app/infocenter/view/[id]/page.tsx
+++ src/app/infocenter/view/[id]/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { useParams } from 'next/navigation'; // 주소창의 [category]를 읽어오는 녀석
 import InfoCenterLayout from "@/components/layout/infocenter/InfoCenterLayout";
-import BoardListTab from "@/components/infocenter/tabs/InfoListTab";
+import InfoListTab from "@/components/infocenter/tabs/InfoListTab";
 
 export default function CategoryListPage() {
   const params = useParams();
@@ -13,7 +13,7 @@ export default function CategoryListPage() {
   return (
     <InfoCenterLayout isDarkMode={isDarkMode}>
       {/* 🌟 핵심: BoardListTab에게 '너는 지금 무슨 게시판이야'라고 주소창 값을 던져줍니다. */}
-      <BoardListTab isDarkMode={isDarkMode} activeTab={category} />
+      <InfoListTab isDarkMode={isDarkMode} activeTab={category} />
     </InfoCenterLayout>
   );
 }
