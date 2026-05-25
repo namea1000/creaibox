5b3737d508d6f39deffa2e2f0223f670161e92cd
namea1000 <jenam7720@gmail.com>
2026-05-25 23:33:51 +0900

    로고 다시

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,19 +14,19 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "크리에이아이박스(CreAIbox) 올인원 AI 콘텐츠 스튜디오",
-  description: "글쓰기, 이미지, 비디오, 뮤직 생성부터 트렌드 분석까지. 상상력을 현실로 만드는 가장 똑똑한 AI 상자, CreAIbox를 경험해 보세요.",
+  title: "크리에이박스(CreAibox) 올인원 AI 콘텐츠 스튜디오",
+  description: "글쓰기, 이미지, 비디오, 뮤직 생성부터 트렌드 분석까지. 상상력을 현실로 만드는 가장 똑똑한 AI 상자, CreAibox를 경험해 보세요.",
   openGraph: {
-    title: "크리에이아이박스 (CreAIbox)",
+    title: "크리에이박스 (CreAibox)",
     description: "상상력을 현실로 만드는 가장 똑똑한 올인원 AI 콘텐츠 플랫폼 | 올인원 AI 콘텐츠 스튜디오 ",
     url: "https://creaibox.com",
-    siteName: "CreAIbox",
+    siteName: "CreAibox",
     images: [
       {
-        url: "https://creaibox.com/logothumbnail.webp",
+        url: "https://creaibox.com/logo.webp",
         width: 1200,
         height: 630,
-        alt: "CreAIbox 메인 로고",
+        alt: "CreAibox 메인 로고",
       },
     ],
     locale: "ko_KR",
