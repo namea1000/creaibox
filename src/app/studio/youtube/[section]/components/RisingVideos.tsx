c190f4bc179a647a58c85db9c1e9397859fa6ba8
namea1000 <jenam7720@gmail.com>
2026-08-18 18:14:48 +0900

    style: hide native calendar icon and stretch click area for better UX

--- src/app/studio/youtube/[section]/components/RisingVideos.tsx
+++ src/app/studio/youtube/[section]/components/RisingVideos.tsx
@@ -675,13 +675,13 @@ const COUNTRY_CODES = new Set(ALL_COUNTRIES.map((c) => c.code));
               </button>
 
               <div className="relative flex items-center h-full">
-                <Calendar size={14} className="absolute left-2.5 text-orange-500 pointer-events-none" />
+                <Calendar size={14} className="absolute left-2.5 text-orange-500 pointer-events-none z-10" />
                 <input
                   type="date"
                   value={selectedDate}
                   onChange={(e) => handleDateChange(e.target.value)}
                   max={getKstTodayDateStr()}
-                  className="h-full w-[130px] border-0 bg-transparent pl-8 pr-1 text-xs font-bold text-slate-800 dark:text-zinc-200 outline-none cursor-pointer dark:[color-scheme:dark]"
+                  className="relative z-20 h-full w-[130px] border-0 bg-transparent pl-8 pr-1 text-xs font-bold text-slate-800 dark:text-zinc-200 outline-none cursor-pointer dark:[color-scheme:dark] [&::-webkit-calendar-picker-indicator]:absolute [&::-webkit-calendar-picker-indicator]:right-0 [&::-webkit-calendar-picker-indicator]:top-0 [&::-webkit-calendar-picker-indicator]:w-full [&::-webkit-calendar-picker-indicator]:h-full [&::-webkit-calendar-picker-indicator]:opacity-0 [&::-webkit-calendar-picker-indicator]:cursor-pointer"
                 />
               </div>
 
